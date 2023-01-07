bfd083bebbabe06c767cbea3dacbfeff081d2a21
Diana Cardona <dscardona@gmail.com>
2023-01-07 12:08:48 -0500

    Create header component and render it

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React from 'react';
 import './App.css';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
 import CampsitesList from './features/campsites/CampsitesList';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-        <Navbar dark color='primary' sticky='top' expand='md'>
-            <Container>
-                <NavbarBrand href='/'>
-                    <img src={NucampLogo} alt='nucamp logo' />
-                </NavbarBrand>
-            </Container>
-        </Navbar>
-        <CampsitesList/>
+      <Header/>
+      <CampsitesList/>
     </div>
   );
 }
