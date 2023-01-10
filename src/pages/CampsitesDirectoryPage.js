9d439145844b2c68e8a524154021d19d03f397d6
Diana Cardona <dscardona@gmail.com>
2023-01-10 15:28:37 -0500

    Add button

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -3,11 +3,19 @@ import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
 import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
 
-const CapsitesDirectoryPage = () => {
-    const selectedCampsite = selectRandomCampsite();
+const CampsitesDirectoryPage = () => {
+    let selectedCampsite = selectRandomCampsite();
+
+    const toggleCampsite = () => {
+        selectedCampsite = selectRandomCampsite();
+        console.log(selectedCampsite);
+    };
 
     return (
         <Container>
+            <Button onClick={()=>toggleCampsite()}>
+                Select Random Campsite
+            </Button>
             <Row>
                 <Col sm='5' md='7'>
                     <CampsitesList />
