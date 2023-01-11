91d0db354248a83aca0a60eee380cd0df066b8a6
Diana Cardona <dscardona@gmail.com>
2023-01-11 14:46:56 -0500

    Create DisplayList component under display feature

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -7,3 +7,7 @@ export const selectAllCampsites = () => {
 export const selectCampsitebyId = (id) => {
     return CAMPSITES.find(campsite => campsite.id === id );
 };
+
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find(campsite => campsite.featured);
+};
