dc676f142066aa1e556922a78eb52c0e92021f4d
Diana Cardona <dscardona@gmail.com>
2023-01-11 12:05:06 -0500

    Implement useState() hook in campsite directory page component

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,6 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 } 
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+export const selectCampsitebyId = (id) => {
+    return CAMPSITES.find(campsite => campsite.id === id );
 };
