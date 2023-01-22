c1faa2d5c7d986f1d3912ae0ee15e46a0171b895
Diana Cardona <dscardona@gmail.com>
2023-01-22 11:06:07 -0500

    Create handleSubmit function within the CommentForm component

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,8 +1,20 @@
 import { useState } from "react";
-import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from "reactstrap";
+import { Formik, Field, Form } from "formik";
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
+    
+    const handleSubmit = (values) => {
+        const comment = {
+            campsiteId: parseInt(campsiteId),
+            rating: values.rating,
+            author: values.author,
+            text: values.commentText
+        };
+        console.log(comment);
+        setModalOpen(false)
+    };
 
     return (
         <>
